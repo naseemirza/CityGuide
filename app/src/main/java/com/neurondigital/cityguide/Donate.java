e2c47eb646e23c86b45861d0a6d997d9e62e41dc
naseem <naseemirza786@gmail.com>
2017-11-07 11:37:36 +0530

    add at sir's home

---
+++ app/src/main/java/com/neurondigital/cityguide/Donate.java
@@ -0,0 +1,54 @@
+package com.neurondigital.cityguide;
+
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+
+public class Donate extends AppCompatActivity {
+
+    WebView mywebview;
+    ProgressDialog progressDialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donate);
+
+        mywebview = (WebView) findViewById(R.id.webView1);
+        mywebview.setWebViewClient(new MyWebViewClient());
+
+        String url="https://www.payumoney.com/paybypayumoney/#/7435FB2321DB6A7E1606E5CD5B941CB7";
+        mywebview.getSettings().setJavaScriptEnabled(true);
+        mywebview.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        mywebview.loadUrl(url);
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            progressDialog = new ProgressDialog(Donate.this);
+            progressDialog.setMessage("Please wait ...");
+            progressDialog.setProgressStyle(90);
+            progressDialog.show();
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            if (progressDialog != null) {
+                progressDialog.dismiss();
+            }
+        }
+    }
+}
--- app/src/main/java/com/neurondigital/cityguide/HomeScreen.java
+++ app/src/main/java/com/neurondigital/cityguide/HomeScreen.java
@@ -1,11 +1,16 @@
 package com.neurondigital.cityguide;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -20,8 +25,11 @@ import ir.mirrajabi.searchdialog.core.Searchable;
 
 public class HomeScreen extends AppCompatActivity {
 //    AppCompatActivity activity;
-    ImageButton imageButton1;
-    Button button_search;
+    ImageButton button_shoping,button_home,button_donate;
+
+    WebView mywebview;
+    ProgressDialog progressDialog;
+    Button button_search ;
 //    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +37,24 @@ public class HomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_home_screen);
 
 
+
+//        mywebview = (WebView) findViewById(R.id.webView1);
+//        mywebview.setWebViewClient(new MyWebViewClient());
+//
+//        String url="http://d24.co.in/";
+//        mywebview.getSettings().setJavaScriptEnabled(true);
+//        mywebview.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+//        mywebview.loadUrl(url);
+
+
+
         button_search=(Button)findViewById(R.id.btn_search);
+
+        button_shoping=(ImageButton)findViewById(R.id.imageButton_shpng);
+        button_home=(ImageButton)findViewById(R.id.imageButton_home);
+        button_donate=(ImageButton)findViewById(R.id.imageButton_donate);
+
+
 
         button_search.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +62,58 @@ public class HomeScreen extends AppCompatActivity {
                 startActivity(new Intent(HomeScreen.this,MainActivity.class));
             }
         });
+
+        button_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        button_shoping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeScreen.this,Shoping.class));
+
+            }
+        });
+
+        button_donate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeScreen.this,Donate.class));
+
+            }
+        });
+
+
+
+//        private class MyWebViewClient extends WebViewClient {
+//            @Override
+//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//                view.loadUrl(url);
+//                return true;
+//            }
+//
+//            @Override
+//            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+//                super.onPageStarted(view, url, favicon);
+//                progressDialog = new ProgressDialog(HomeScreen.this);
+//                progressDialog.setMessage("Please wait ...");
+//                progressDialog.setProgressStyle(90);
+//                progressDialog.show();
+//            }
+//            @Override
+//            public void onPageFinished(WebView view, String url) {
+//                super.onPageFinished(view, url);
+//                if (progressDialog != null) {
+//                    progressDialog.dismiss();
+//                }
+//            }
+//        }
+
+
+
 
 //        editText=(EditText)findViewById(R.id.mainFragment);
 
